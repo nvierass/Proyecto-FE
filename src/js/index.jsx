c3d65356eadf2607ff4b32a18cec39ee94f53e3a
nvierass <nicolasvieras.s@gmail.com>
2024-12-14 19:47:14 -0300

    Git ignore settings to exclude API Keys

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
+import { API_KEY_MARVEL } from "./config/marvelApi.secrets";
 
 const root = createRoot(document.getElementById("app"));
 
 root.render(<h1>Hello World!</h1>)
+
