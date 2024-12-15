6e5a6094478aa95a4f402d861ddb9815233ddd3f
nvierass <nicolasvieras.s@gmail.com>
2024-12-15 17:34:08 -0300

    Feat: SearchBar component added

---
+++ src/js/components/SearchBar.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+
+export default function SearchBar({searchCallback}) {
+
+    const [inputText, setInputText] = useState("");
+    const [informativeLabel, setInformativeLabel] = useState("")
+
+    const searchHandler = (e) => {
+        e.preventDefault();
+        if (!inputText){
+            setInformativeLabel("Debe ingresar al menos un término para buscar.");
+        }
+        else {
+            setInformativeLabel("");
+            searchCallback(inputText);
+        }
+    }
+
+    const updateHandler = (e) => {
+        e.preventDefault();
+        setInputText(e.target.value);
+    }
+
+    return (
+        <div className="search-bar">
+            <span className = "search-bar__label"> Busca obras de tus artistas favoritos! </span>
+            <div className = "search-bar__input-wrapper">
+                <input placeholder = "Van Gogh, Picasso, Dalí..." onChange={updateHandler}></input>
+                <button className = "search-bar__input-wrapper__button" type = "submit" onClick = {searchHandler}> 
+                    <img width = "15px" height = "15px" src = "./src/assets/icons/magnifying-glass-solid.svg"/>
+                </button>
+            </div>
+            <span className = "search-bar__error-label"> {informativeLabel}</span>
+        </div>
+    )
+}
+
+
+
