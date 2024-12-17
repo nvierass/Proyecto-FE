da48650138608d0fedf02f0a3e767b458021f2fe
nvierass <nicolasvieras.s@gmail.com>
2024-12-16 23:55:16 -0300

    Fix: Fetching data - Now the GET request includes the params correctly. - Fixed image url generation.

--- src/js/service/ArtworkApiTransactions.js
+++ src/js/service/ArtworkApiTransactions.js
@@ -9,25 +9,31 @@ function getRandomInt(minimum, maximum){
 }
 
 async function fetchArtworks(){
-    let offsetParam = "?offset=" + generateRandomOffset();
-    let fieldsParam = "?fields=id,title,image_id,description";
-    const response = await fetch(URL + offsetParam + fieldsParam);
-    return await response.json();
+    let offsetParam = "offset=0" + generateRandomOffset();
+    let fieldsParam = "&fields=id,title,image_id,description";
+    const response = await fetch(URL + "?" + offsetParam + fieldsParam);
+    const responseJson = await response.json();
+
+    return {data: responseJson.data, imageRepositoryInfo: responseJson.config}
 }
 
-function buildArtworksImages(artworksRawData){
-    const IIIF_URL = artworksData.config.iiif_url;
-    const IMAGE_FORMAT = "/full/843,/0/default.jpg";
-    return artworksRawData.data.map((artwork) => {
-        artwork.imageUrl = IIIF_URL + artwork.image_id + IMAGE_FORMAT;
-        return artwork;
-    })
-}
 
-export const getArtworkCollection = async () => {
-    let artworksRawData = await fetchArtworks();
-    console.log(artworksRawData);
-    let artworkDataWithImages = buildArtworksImages(artworksRawData);
+
+export const getRandomArtworksGallery = async () => {
+    let artworksData = await fetchArtworks();
+    let artworkDataWithImages = buildArtworksImages(artworksData);
     
-
 }
+
+function buildArtworksImages(artworksData){
+    const IIIF_URL = artworksData.imageRepositoryInfo.iiif_url;
+    const IMAGE_FORMAT = "/full/843,/0/default.jpg";
+    artworksData.data.forEach((artwork) => {
+        artwork.image_url = IIIF_URL + "/" + artwork.image_id + IMAGE_FORMAT;
+    });
+    console.log(artworksData.data)
+}
+
+
+const x = (await getRandomArtworksGallery())
+
