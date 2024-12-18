5e82e7c6a41d0bffdad4d704495329bba167906f
nvierass <nicolasvieras.s@gmail.com>
2024-12-17 23:32:42 -0300

    Designs updates

--- src/js/components/main.jsx
+++ src/js/components/main.jsx
@@ -1,21 +1,22 @@
 import { useState, useEffect } from "react";
 import SearchBar from "./searchBar";
 import { getRandomArtworksGallery, getQueryArtworksGallery } from "../service/ArtworkApiTransactions";
+import ArtworkCard from "./ArtworkCard";
 
 export default function Main({}){
-    const [currentGallery, setCurrentGallery] = useState([]);
+    const [gallery, setGallery] = useState([]);
     const [isSearchResult, setIsSearchResult] = useState();
     const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(()=> {
-        getRandomArtworksGallery().then(gallery => setCurrentGallery(gallery));
+        getRandomArtworksGallery().then(gallery => setGallery(gallery));
     },[])
 
     const searchHandler = (query) => {
         if(query){
             setIsSearchResult(true);
             setSearchQuery(query);
-            getQueryArtworksGallery(searchQuery).then(gallery => setCurrentGallery(gallery));
+            getQueryArtworksGallery(searchQuery).then(gallery => setGallery(gallery));
         }
     }
 
@@ -23,11 +24,18 @@ export default function Main({}){
         <main className="main">
             <SearchBar searchCallback={searchHandler}/>
             <section className="main__artworks-section">
-            {isSearchResult? 
-            <h1 className="main__artworks-section__title"> Resultados de la busqueda "{searchQuery}" </h1>:
-            <h1 className="main__artworks-section__title"> Obras destacadas: </h1>
-            }
-                    
+                {isSearchResult? 
+                <h1 className="main__artworks-section__title"> Resultados de la busqueda "{searchQuery}" </h1>:
+                <h1 className="main__artworks-section__title"> A continuación te presentamos una selección de obras destacadas</h1>
+                }
+                <ul className="main__artworks-section__artworks-list">
+                    {gallery.map((artwork, index) => 
+                        <li className = "main__artworks-section__artworks-list__item" key = {index} >
+                            <ArtworkCard id = {artwork.id} title = {artwork.title } imageUrl = {artwork.image_url} description = {artwork.description}/>
+                        </li>
+                    )}
+
+                </ul>
             </section>
 
 
